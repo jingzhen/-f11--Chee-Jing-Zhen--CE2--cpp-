dc6bff1ee008c9f40c1f06cea83ccaceb3d8dca1
jingzhen <cheejingzhen@hotmail.com>
2015-02-28 00:43:41 +0800

    Method for commandDisplay

--- Text Buddy 2/Text Buddy 2/TextBuddy.cpp
+++ Text Buddy 2/Text Buddy 2/TextBuddy.cpp
@@ -9,21 +9,35 @@
 
 using namespace std;
 
+const string welcome = "Welcome to TextBuddy. ";
+const string ready = " is ready for use";
+const string added = "added to ";
+const string punctuation1 = ": \"";
+const string punctuation2 = "\"";
+const string punctuation3 = ". ";
+
+
 TextBuddy::TextBuddy(string fileName){
 	_fileName = fileName;
 }
 
 void TextBuddy::displayMessage(){
-	cout << "Welcome to TextBuddy. " << _fileName << " is ready for use" << endl;
+	cout << welcome << _fileName << ready << endl;
 }
 
 void TextBuddy::commandAdd(string newDescription){
 	getchar();
 	getline (cin, newDescription);
 	_descriptionStorage.push_back(newDescription);
-	cout << "added to " << _fileName << ": \"" << newDescription << "\"" << endl;
+	cout << added << _fileName << punctuation1 << newDescription << punctuation2 << endl;
 }
-void TextBuddy::commandDisplay(){}
+
+void TextBuddy::commandDisplay(){
+	for (int countSentence=0; countSentence<(int)_descriptionStorage.size(); countSentence++){
+		cout << countSentence+1 << punctuation3 << _descriptionStorage[countSentence] << endl;
+	}
+}
+
 void TextBuddy::commandDelete(){}
 void TextBuddy::commandClear(){}
 void TextBuddy::commandExit(){}
